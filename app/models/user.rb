1058b6f164ec8abc0bec1dc4762d9de9fa49dfd6
yamada7717 <hiro.program17@gmail.com>
2023-05-27 09:04:50 +0900

    active_storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  #ユーザーイメージの追加
+  has_one_attached :user_image
 end
