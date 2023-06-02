870486ec9d0b491dd6b3c7b52ac61d9184a8717b
yamada7717 <hiro.program17@gmail.com>
2023-06-02 12:12:24 +0900

    Roomコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/edit'
+  get 'rooms/create'
+  get 'rooms/update'
+  get 'rooms/destroy'
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
