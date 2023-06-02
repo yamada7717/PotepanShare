eeb7ff9e562fa71a5987cb19b80057068c85f657
yamada7717 <hiro.program17@gmail.com>
2023-06-02 12:14:50 +0900

    Reservationコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/show'
+  get 'reservations/new'
+  get 'reservations/edit'
+  get 'reservations/create'
+  get 'reservations/update'
+  get 'reservations/destroy'
   get 'rooms/index'
   get 'rooms/show'
   get 'rooms/new'
