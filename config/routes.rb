932b167449a278460184c1cd43824ab358685fc2
yamada7717 <hiro.program17@gmail.com>
2023-06-03 17:19:50 +0900

    ユーザー登録、編集に関するページの編集。不必要な文言の消去

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,8 @@ Rails.application.routes.draw do
   get 'reservations/create'
   get 'reservations/update'
   get 'reservations/destroy'
-  get 'rooms/index'
-  get 'rooms/show'
-  get 'rooms/new'
-  get 'rooms/edit'
-  get 'rooms/create'
-  get 'rooms/update'
-  get 'rooms/destroy'
   devise_for :users
   root to: "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms
+  resources :reservations
 end
