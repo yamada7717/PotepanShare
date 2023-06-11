1c4f86a5da320e3c6ca49b3ce8ec6c46f253a580
yamada7717 <hiro.program17@gmail.com>
2023-06-12 05:30:51 +0900

    予約ページの編集

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,12 +6,23 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @room = Room.find(params[:room_id])
+    @reservation = Reservation.new
   end
 
   def edit
   end
 
   def create
+    @room = Room.find(params[:room_id])
+    @reservation = @room.reservations.build(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to reservation_confirmation_path(@reservation)
+    else
+      render :new
+    end
   end
 
   def update
