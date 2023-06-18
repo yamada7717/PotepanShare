857df990b32276c5459854fd96bbd68447628951
yamada7717 <hiro.program17@gmail.com>
2023-06-19 00:02:32 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+
+  validates :user_name, presence: true
   #ユーザーイメージの追加
   has_many :rooms
   has_many :reservations
